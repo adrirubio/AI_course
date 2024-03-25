1bbdefad7beedcc66c8a891705baeb9796d5a3ac
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-03-25 15:15:02 +0100

    Auto commit on lun 25 mar 2024 15:15:02 CET

---
+++ NLP/NLP.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import requests
+import io
+
+url = 'https://lazyprogrammer.me/course_files/spam.csv'
+response = requests.get(url)
+response.raise_for_status()  # Raise an error if download fails
+
+df = pd.read_csv(io.StringIO(response.content.decode('latin-1')), encoding='latin-1')
+
+df = df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
+
+df.columns = ['labels', 'data']
+
+# create binary labels
+df['b_labels'] = df['labels'].map({'ham': 0, 'spam': 1})
+df_train, df_test = train_test_split(df, test_size=0.33)
+
+# 0 = padding
+idx = 1
+word2idx = {'<PAD>': 0}
+# you could always use gensim or spacy for tokenization,
+# but let's keep it simple!
+for i, row in df_train.iterrows():
+  tokens = row['data'].lower().split() # simple tokenization
+  for token in tokens:
+    if token not in word2idx:
+      word2idx[token] = idx
+      idx += 1
+
+
+# convert data into word indices
+# note: could have done this on the fly earlier
+train_sentences_as_int = []
+for i, row in df_train.iterrows():
+  tokens = row['data'].lower().split()
+  sentence_as_int = [word2idx[token] for token in tokens]
+  train_sentences_as_int.append(sentence_as_int)
+
+test_sentences_as_int = []
+for i, row in df_test.iterrows():
+  tokens = row['data'].lower().split()
+  sentence_as_int = [word2idx[token] for token in tokens if token in word2idx]
+  test_sentences_as_int.append(sentence_as_int)
+
+
+def data_generator(X, y, batch_size=32):
+  X, y = shuffle(X, y)
+  n_batches = int(np.ceil(len(y) / batch_size))
+  for i in range(n_batches):
+    end = min((i + 1) * batch_size, len(y))
+
+    X_batch = X[i * batch_size:end]
+    y_batch = y[i * batch_size:end]
+
+    # pad X_batch to be N x T
+    max_len = np.max([len(x) for x in X_batch])
+    for j in range(len(X_batch)):
+      x = X_batch[j]
+      pad = [0] * (max_len - len(x))
+      X_batch[j] = pad + x
+
+    # convert to tensor
+    X_batch = torch.from_numpy(np.array(X_batch)).long()
+    y_batch = torch.from_numpy(np.array(y_batch)).long()
+
+    yield X_batch, y_batch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
