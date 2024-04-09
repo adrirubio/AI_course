9787f9a9e88ec9c50f8977734388a107f0b8399d
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-04-09 18:55:02 +0200

    Auto commit on mar 09 abr 2024 18:55:02 CEST

--- Recommender_Systems/recommender.py
+++ Recommender_Systems/recommender.py
@@ -11,5 +11,3 @@ import io
 
 url ='http://files.grouplens.org/datasets/movielens/ml-20m.zip'
 response = requests.get(url)
-#df = pd.read_csv(io.StringIO(response.content.decode('latin-1')), encoding='latin-1')
-
