f1b67ba998789656da3bfec84dcdce8f6749efa1
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-04-15 21:30:03 +0200

    Auto commit on lun 15 abr 2024 21:30:03 CEST

--- Recommender_Systems/recommender.py
+++ Recommender_Systems/recommender.py
@@ -28,3 +28,46 @@ df['new_user_id'] = df.userId.cat.codes
 
 df.movieId = pd.Categorical(df.movieId)
 df['new_movie_id'] = df.movieId.cat.codes
+
+# Get user IDs, movie IDs, and ratings as separate arrays
+user_ids = df['new_user_id'].values
+movie_ids = df['new_movie_id'].values
+ratings = df['rating'].values - 2.5
+
+# Get number of users and number of movies
+N = len(set(user_ids))
+M = len(set(movie_ids))
+
+# Set embedding dimension
+# Using embeddings just like in NLP
+D = 10
+
+# Make a neural network
+class Model(nn.Module):
+  def __init__(self, n_users, n_items, embed_dim, n_hidden=1024):
+    super(Model, self).__init__()
+    self.N = n_users
+    self.M = n_items
+    self.D = embed_dim
+
+    self.u_emb = nn.Embedding(self.N, self.D)
+    self.m_emb = nn.Embedding(self.M, self.D)
+    self.fc1 = nn.Linear(2 * self.D, n_hidden)
+    self.fc2 = nn.Linear(n_hidden, 1)
+
+  def forward(self, u, m):
+    u = self.u_emb(u) # output is (num_samples, D)
+    m = self.m_emb(m) # output is (num_samples, D)
+
+    # merge
+    out = torch.cat((u, m), 1) # output is (num_samples, 2D)
+
+    # ANN
+    out = self.fc1(out)
+    out = F.relu(out)
+    out = self.fc2(out)
+    return out
+
+model = Model(N, M, D)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
