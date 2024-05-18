30489468d6035b69fb8dd7d4491ffaf4a5ceedb4
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-05-18 14:35:01 +0200

    Auto commit on sáb 18 may 2024 14:35:01 CEST

---
+++ Transfer_learning/transfer_learning_code.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import datasets, transforms, models
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import sys, os
+from glob import glob
+import imageio
+
+
