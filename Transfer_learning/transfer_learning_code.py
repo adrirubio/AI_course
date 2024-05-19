9b43324c2f81b88dc1be8f5f04d027841fec4f3b
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-05-19 18:00:02 +0200

    Auto commit on dom 19 may 2024 18:00:02 CEST

--- Transfer_learning/transfer_learning_code.py
+++ Transfer_learning/transfer_learning_code.py
@@ -10,3 +10,7 @@ from glob import glob
 import imageio
 
 
+# Data from: https://mmspg.epfl.ch/downloads/food-image-datasets/
+# !wget --passive-ftp --prefer-family=ipv4 --ftp-user FoodImage@grebvm2.epfl.ch \
+#  --ftp-password Cahc1moo -nc ftp://tremplin.epfl.ch/Food-5K.zip
+!wget -nc https://archive.org/download/food-5-k/Food-5K.zip
