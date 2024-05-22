1927d2dad1fcf5ade7100ff2c961bb12b21d735b
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2024-05-22 18:05:02 +0200

    Auto commit on mié 22 may 2024 18:05:02 CEST

--- Transfer_learning/transfer_learning_code.py
+++ Transfer_learning/transfer_learning_code.py
@@ -171,3 +171,166 @@ for inputs, targets in test_loader:
 
 test_acc = n_correct / n_total
 print(f"Train acc: {train_acc:.4f}, Test acc: {test_acc:.4f}")
+
+image_path = '/home/adrian/data/train/nonfood/0_951.jpg'
+
+plt.imshow(imageio.imread(image_path))
+# remove # to see image
+# plt.show()
+
+image_path = '/home/adrian/data/train/food/1_293.jpg'
+
+plt.imshow(imageio.imread(image_path))
+# remove # to see image
+# plt.show()
+
+# Note: normalize mean and std are standardized for ImageNet
+# https://github.com/pytorch/examples/blob/97304e232807082c2e7b54c597615dc0ad8f6173/imagenet/main.py#L197-L198
+train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+        transforms.RandomRotation(degrees=15),
+        transforms.ColorJitter(),
+        transforms.CenterCrop(size=224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+test_transform = transforms.Compose([
+        transforms.Resize(size=256),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+train_dataset = datasets.ImageFolder(
+    '/home/adrian/data/train',
+    transform=train_transform
+)
+test_dataset = datasets.ImageFolder(
+    '/home/adrian/data/test',
+    transform=test_transform
+)
+
+batch_size = 128
+train_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    shuffle=True
+)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=batch_size,
+)
+
+
+# Define the model
+model = models.vgg16(pretrained=True)
+
+# Freeze VGG weights
+for param in model.parameters():
+  param.requires_grad = False
+
+n_features = model.classifier[0].in_features
+# We're doing binary classification
+model.classifier = nn.Linear(n_features, 2)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+def batch_gd(model, criterion, optimizer, train_loader, test_loader, epochs):
+  train_losses = np.zeros(epochs)
+  test_losses = np.zeros(epochs)
+
+  for it in range(epochs):
+    t0 = datetime.now()
+    train_loss = []
+    for inputs, targets in train_loader:
+      # move data to GPU
+
+      # zero the parameter gradients
+      optimizer.zero_grad()
+
+      # Forward pass
+      outputs = model(inputs)
+      loss = criterion(outputs, targets)
+
+      # Backward and optimize
+      loss.backward()
+      optimizer.step()
+
+      train_loss.append(loss.item())
+
+    # Get train loss and test loss
+    train_loss = np.mean(train_loss) # a little misleading
+
+    test_loss = []
+    for inputs, targets in test_loader:
+      outputs = model(inputs)
+      loss = criterion(outputs, targets)
+      test_loss.append(loss.item())
+    test_loss = np.mean(test_loss)
+
+    # Save losses
+    train_losses[it] = train_loss
+    test_losses[it] = test_loss
+
+    dt = datetime.now() - t0
+    print(f'Epoch {it+1}/{epochs}, Train Loss: {train_loss:.4f}, \
+      Test Loss: {test_loss:.4f}, Duration: {dt}')
+
+  return train_losses, test_losses
+
+train_losses, test_losses = batch_gd(
+    model,
+    criterion,
+    optimizer,
+    train_loader,
+    test_loader,
+    epochs=5,
+)
+
+# Plot the train loss and test loss per iteration
+plt.plot(train_losses, label='train loss')
+plt.plot(test_losses, label='test loss')
+plt.legend()
+plt.show()
+
+# Accuracy
+
+n_correct = 0.
+n_total = 0.
+for inputs, targets in train_loader:
+
+  # Forward pass
+  outputs = model(inputs)
+
+  # Get prediction
+  # torch.max returns both max and argmax
+  _, predictions = torch.max(outputs, 1)
+
+  # update counts
+  n_correct += (predictions == targets).sum().item()
+  n_total += targets.shape[0]
+
+train_acc = n_correct / n_total
+
+
+n_correct = 0.
+n_total = 0.
+for inputs, targets in test_loader:
+
+  # Forward pass
+  outputs = model(inputs)
+
+  # Get prediction
+  # torch.max returns both max and argmax
+  _, predictions = torch.max(outputs, 1)
+
+  # update counts
+  n_correct += (predictions == targets).sum().item()
+  n_total += targets.shape[0]
+
+test_acc = n_correct / n_total
+print(f"Train acc: {train_acc:.4f}, Test acc: {test_acc:.4f}")
